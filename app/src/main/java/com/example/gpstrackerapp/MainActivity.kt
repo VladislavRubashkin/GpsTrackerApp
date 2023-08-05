c2068302a9b05728955b92a828a5b6ec02a6fee0
Vladislav <RubashkinVlad@yandex.ru>
2023-08-05 21:00:43 +0300

    Add dependencies

--- app/src/main/java/com/example/gpstrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/gpstrackerapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // TODO#1 - branches: architecture -> mainMenu -> navigationComponent -> и тд
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
